e6372766633dd4506c5027103df4dc24a3d7b98c
terranivium <wesleyscottuk@gmail.com>
2020-03-16 01:22:01 +0000

    Server and Clients working, todo: implement game logic

--- AssEx/Client.java
+++ AssEx/Client.java
@@ -1,42 +1,175 @@
-import java.net.Socket;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Scanner;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class Client{
-    public static void main(String[] args) {
-        String output;
-        Socket serverConnect = null;
-        try {
-            serverConnect = new Socket("127.0.0.1", 8765);
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-            PrintWriter outToServer =
-                new PrintWriter(serverConnect.getOutputStream(), true);
-            Scanner inFromServer =
-                new Scanner(
-                    new InputStreamReader(serverConnect.getInputStream()));
-            BufferedReader inFromUser = 
-                new BufferedReader(
-                    new InputStreamReader(System.in));
+public class Client {
 
-            while(inFromServer.hasNext()){
-                output = inFromServer.nextLine();
-                System.out.println(output);
+    private JFrame frame = new JFrame("Checkers");
+    private JLabel messageLabel = new JLabel("...");
+
+    private Square[][] board = new Square[8][8];
+    private Square moveFrom;
+    int fromCol;
+    int fromRow;
+    private Square moveTo;
+    private int redCheckers = 12;
+    private int blackCheckers = 12;
+
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+
+    private boolean mousePressed = false;
+
+    public Client(String serverAddress) throws Exception {
+
+        socket = new Socket(serverAddress, 58901);
+        in = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        messageLabel.setBackground(Color.lightGray);
+        frame.getContentPane().add(messageLabel, BorderLayout.SOUTH);
+
+        var boardPanel = new JPanel();
+        boardPanel.setBackground(Color.black);
+        boardPanel.setBounds(0,0,600,600);
+        boardPanel.setLayout(new GridLayout(8, 8, 2, 2));
+        for (var i = 0; i < board[0].length; i++) {
+            final int k = i;
+            for (var j = 0; j < board[0].length; j++) {
+                final int l = j;
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                    board[i][j] = new Square(i, j);
+                    board[i][j].setColour(true);
+                } else {
+                    board[i][j] = new Square(i, j);
+                    board[i][j].setColour(false);
+                }
+                boardPanel.add(board[k][l]);
             }
-
-            inFromUser.close();
-            inFromServer.close();
-            outToServer.close();
-
-            System.out.println();
-            System.out.println("----------------------");
-            System.out.println("Thank you for playing!");
-            System.out.println("----------------------");
-
-            serverConnect.close(); // close the socket
-        } catch(Exception e) {
-            e.printStackTrace();
+            frame.getContentPane().add(boardPanel, BorderLayout.CENTER);
         }
-    }    
+        boardPanel.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                    fromCol = e.getX() / 75;
+                    fromRow = e.getY() / 72;
+                    moveFrom = board[fromRow][fromCol];
+            }
+            public void mouseReleased(MouseEvent e){
+                    int col = e.getX() / 75;
+                    int row = e.getY() / 72;
+                    moveTo = board[row][col];
+                    out.println("MOVE " + fromRow + fromCol + row + col);
+                    System.out.println("MOVE " + fromRow + fromCol + row + col);
+            }
+        });
+    }
+
+    /**
+     * The main thread of the client will listen for messages from the server. The
+     * first message will be a "WELCOME" message in which we receive our mark. Then
+     * we go into a loop listening for any of the other messages, and handling each
+     * message appropriately. The "VICTORY", "DEFEAT", "TIE", and
+     * "OTHER_PLAYER_LEFT" messages will ask the user whether or not to play another
+     * game. If the answer is no, the loop is exited and the server is sent a "QUIT"
+     * message.
+     */
+    public void play() throws Exception {
+        try {
+            var response = in.nextLine();
+            var mark = response.charAt(8);
+            var opponentMark = mark == 'r' ? 'b' : 'r';
+            frame.setTitle("Checkers: Player " + mark);
+            while (in.hasNextLine()) {
+                response = in.nextLine();
+                if (response.startsWith("VALID_MOVE")) {
+                    messageLabel.setText("Valid move, please wait");
+                    moveFrom.setText(' ');
+                    moveTo.setText(mark);
+                    moveFrom.repaint();
+                    moveTo.repaint();
+                } else if (response.startsWith("OPPONENT_MOVED")) {
+                    var loc1 = Integer.parseInt(response.substring(15,16));
+                    var loc2 = Integer.parseInt(response.substring(16,17));
+                    board[loc1][loc2].setText(opponentMark);
+                    board[loc1][loc2].repaint();
+                    messageLabel.setText("Opponent moved, your turn");
+                } else if (response.startsWith("MESSAGE")) {
+                    messageLabel.setText(response.substring(8));
+                } else if (response.startsWith("VICTORY")) {
+                    JOptionPane.showMessageDialog(frame, "Winner Winner");
+                    break;
+                } else if (response.startsWith("DEFEAT")) {
+                    JOptionPane.showMessageDialog(frame, "Sorry you lost");
+                    break;
+                } else if (response.startsWith("TIE")) {
+                    JOptionPane.showMessageDialog(frame, "Tie");
+                    break;
+                } else if (response.startsWith("OTHER_PLAYER_LEFT")) {
+                    JOptionPane.showMessageDialog(frame, "Other player left");
+                    break;
+                }
+            }
+            out.println("QUIT");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+            frame.dispose();
+        }
+    }
+
+    static class Square extends JPanel {
+        JLabel label = new JLabel();
+        int i;
+        int j;
+
+        public Square(int i, int j) {
+            setLayout(new GridBagLayout());
+            label.setFont(new Font("Arial", Font.BOLD, 40));
+            add(label);
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        public void setColour(boolean squareColour){
+            if(squareColour){
+                setBackground(Color.white);
+            } else setBackground(Color.black);
+        }
+
+        public void setText(char text) {
+            label.setForeground(text == 'r' ? Color.RED : Color.BLUE);
+            label.setText(text + "");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client("127.0.0.1");
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setSize(600, 600);
+        client.frame.setVisible(true);
+        client.frame.setResizable(false);
+        client.play();
+    }
 }
--- AssEx/Server.java
+++ AssEx/Server.java
@@ -1,46 +1,137 @@
-import java.net.Socket;
-import java.net.ServerSocket;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
 
-public class Server{      
-    public static void main(String[] args) {
-        ServerSocket listener = null;
-        Socket client1 = null;
-        Socket client2 = null;
-        try {
-            listener = new ServerSocket(8765);
-            System.out.println("------------------------------");
-            System.out.println("|Checkers Server CLI ver. 1.0|");
-            System.out.println("------------------------------");
+public class Server{
 
-            client1 = listener.accept();
-            System.out.println();
-            System.out.println("Player One connected!");
-            System.out.println("Waiting for second player...");
-
-            client2 = listener.accept();
-            System.out.println();
-            System.out.println("Player Two connected!");
-            
-            Thread game = new Thread(new ServerThread(client1, client2));
-            game.start();
-            
-            try {
-                game.join();
-            } catch(InterruptedException e){
-                e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        try (var listener = new ServerSocket(58901)) {
+            System.out.println("Checkers Server is Running...");
+            var pool = Executors.newFixedThreadPool(200);
+            while (true) {
+                Game game = new Game();
+                pool.execute(game.new Player(listener.accept(), 'r'));
+                System.out.println("Player 1 connected...");
+                pool.execute(game.new Player(listener.accept(), 'b'));
+                System.out.println("Player 2 connected...");
             }
-
-            System.out.println();
-            System.out.println("-------------------------");
-            System.out.println("Server session closing...");
-            System.out.println("-------------------------");
-            listener.close();
-        } catch(Exception e){
-            e.printStackTrace(); 
         }
-    }    
+    }
+}
+
+class Game {
+
+    // Board cells numbered 0-8, top to bottom, left to right; null if empty
+    private Player[][] board = new Player[8][8];
+    Player currentPlayer;
+
+    public boolean hasWinner() {
+        return (board[0] != null && board[0] == board[1] && board[0] == board[2])
+                || (board[3] != null && board[3] == board[4] && board[3] == board[5])
+                || (board[6] != null && board[6] == board[7] && board[6] == board[8])
+                || (board[0] != null && board[0] == board[3] && board[0] == board[6])
+                || (board[1] != null && board[1] == board[4] && board[1] == board[7])
+                || (board[2] != null && board[2] == board[5] && board[2] == board[8])
+                || (board[0] != null && board[0] == board[4] && board[0] == board[8])
+                || (board[2] != null && board[2] == board[4] && board[2] == board[6]);
+    }
+
+    public synchronized void move(int moveFrom1, int moveFrom2, int moveTo1, int moveTo2, Player player) {
+        // Game logic handled here
+        if (player != currentPlayer) {
+            throw new IllegalStateException("Not your turn");
+        } else if (player.opponent == null) {
+            throw new IllegalStateException("You don't have an opponent yet");
+        } else if (board[moveTo1][moveTo1] != null) {
+            throw new IllegalStateException("Cell already occupied");
+//        } else if (board[moveFrom1][moveFrom2] == null) {
+//            throw new IllegalStateException("Cell has no piece");
+//        }
+        }
+        board[moveTo1][moveTo2] = currentPlayer;
+        currentPlayer = currentPlayer.opponent;
+    }
+
+    /**
+     * A Player is identified by a character mark which is either 'X' or 'O'. For
+     * communication with the client the player has a socket and associated Scanner
+     * and PrintWriter.
+     */
+    class Player implements Runnable {
+        char mark;
+        Player opponent;
+        Socket socket;
+        Scanner input;
+        PrintWriter output;
+
+        public Player(Socket socket, char mark) {
+            this.socket = socket;
+            this.mark = mark;
+        }
+
+        @Override
+        public void run() {
+            try {
+                setup();
+                processCommands();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (opponent != null && opponent.output != null) {
+                    opponent.output.println("OTHER_PLAYER_LEFT");
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        private void setup() throws IOException {
+            input = new Scanner(socket.getInputStream());
+            output = new PrintWriter(socket.getOutputStream(), true);
+            output.println("WELCOME " + mark);
+            if (mark == 'r') {
+                currentPlayer = this;
+                output.println("MESSAGE Waiting for opponent to connect");
+            } else {
+                opponent = currentPlayer;
+                opponent.opponent = this;
+                opponent.output.println("MESSAGE Your move");
+            }
+        }
+
+        private void processCommands() {
+            while (input.hasNextLine()) {
+                var command = input.nextLine();
+                if (command.startsWith("QUIT")) {
+                    return;
+                } else if (command.startsWith("MOVE")) {
+                    System.out.println(Integer.parseInt(command.substring(6, 7)));
+                    processMoveCommand(Integer.parseInt(command.substring(5, 6)), Integer.parseInt(command.substring(6, 7)), Integer.parseInt(command.substring(7, 8)), Integer.parseInt(command.substring(8, 9)));
+                }
+            }
+        }
+
+        private void processMoveCommand(int moveFrom1, int moveFrom2, int moveTo1, int moveTo2) {
+            try {
+                move(moveFrom1, moveFrom2, moveTo1, moveTo2, this);
+                output.println("VALID_MOVE");
+                opponent.output.println("OPPONENT_MOVED " + moveTo1 + moveTo2);
+//                if (hasWinner()) {
+//                    output.println("VICTORY");
+//                    opponent.output.println("DEFEAT");
+//                } else if (boardFilledUp()) { no possible moves
+//                    output.println("TIE");
+//                    opponent.output.println("TIE");
+//                }
+            } catch (IllegalStateException e) {
+                output.println("MESSAGE " + e.getMessage());
+            }
+        }
+    }
 }
